8bbaf33e6cd646cf2ed735e7a37f54853fad1149
Mark Oral Hernandez <blackcoder41@gmail.com>
2019-02-12 22:20:27 +0800

    this is where we begin

---
+++ src/main/java/com/raketlabs/postgresql/SpringJpaApplication.java
@@ -0,0 +1,16 @@
+package com.raketlabs.postgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class SpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/raketlabs/postgresql/repository/QuestionRepository.java
@@ -0,0 +1,9 @@
+package com.raketlabs.postgresql.repository;
+
+import com.raketlabs.postgresql.model.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+}
