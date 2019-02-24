8a11496278e2fbdec876901bb766b43b118efeba
Mark Oral Hernandez <blackcoder41@gmail.com>
2019-02-24 14:27:05 +0800

    Merge changes

--- src/main/java/com/raketlabs/postgresql/configuration/SecurityConfiguration.java
+++ src/main/java/com/raketlabs/postgresql/configuration/SecurityConfiguration.java
@@ -20,57 +20,46 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
-	
+
 	@Autowired
 	private DataSource dataSource;
-	
+
 	private final String AdminUser_Query = "select email, password, activity from user where email=?";
 	private final String AdminRole_Query = "select u.email, r.role from adminuser u inner join role "
 			+ "ur on (u.id = ur.adminuser_id) inner join role r on (ur.id = r.id) where u.email=?";
-	
-	@Override
-	 protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-	  auth.jdbcAuthentication()
-	   .usersByUsernameQuery(AdminUser_Query)
-	   .authoritiesByUsernameQuery(AdminRole_Query)
-	   .dataSource(dataSource)
-	   .passwordEncoder(bCryptPasswordEncoder);
-	 }
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception{
-		
 
-		
-		http.authorizeRequests()
-			.antMatchers("/**").permitAll()
-			.antMatchers("/login").permitAll()
-			.antMatchers("/login2").permitAll()
-			.antMatchers("/register").permitAll()
-			.antMatchers("/view_questions").permitAll()
-			.antMatchers("/signup").permitAll()
-			.antMatchers("/css/**").permitAll().anyRequest().permitAll()
-			.antMatchers("/js/**").permitAll().anyRequest().permitAll()
-			.antMatchers("/home/**").hasAuthority("ADMIN").anyRequest()
-			.authenticated().and().csrf().disable()
-			.formLogin().loginPage("/login").failureUrl("/login?error=true")
-			.defaultSuccessUrl("/home/home")
-			.usernameParameter("email")
-			.passwordParameter("password")
-			.and().logout()
-			.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-			.logoutSuccessUrl("/foo")
-			.and().rememberMe()
-			.tokenRepository(persistentTokenRepository())
-			.tokenValiditySeconds(60*60)
-			.and().exceptionHandling().accessDeniedPage("/access_denied");
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().usersByUsernameQuery(AdminUser_Query).authoritiesByUsernameQuery(AdminRole_Query)
+				.dataSource(dataSource).passwordEncoder(bCryptPasswordEncoder);
 	}
-	
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests()
+		.antMatchers("/**").permitAll()
+		.antMatchers("/login").permitAll()
+		.antMatchers("/login2").permitAll()
+		.antMatchers("/register").permitAll()
+		.antMatchers("/view_questions").permitAll()
+		.antMatchers("/signup").permitAll()
+		.antMatchers("/css/**").permitAll().anyRequest().permitAll()
+		.antMatchers("/js/**").permitAll().anyRequest().permitAll()
+		.antMatchers("/home/**")
+				.hasAuthority("ADMIN").anyRequest().authenticated().and().csrf().disable().formLogin()
+				.loginPage("/login").failureUrl("/login?error=true").defaultSuccessUrl("/home/home")
+				.usernameParameter("email").passwordParameter("password").and().logout()
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/foo").and().rememberMe()
+				.tokenRepository(persistentTokenRepository()).tokenValiditySeconds(60 * 60).and().exceptionHandling()
+				.accessDeniedPage("/access_denied");
+	}
+
 	@Bean
-	 public PersistentTokenRepository persistentTokenRepository() {
-	  JdbcTokenRepositoryImpl db = new JdbcTokenRepositoryImpl();
-	  db.setDataSource(dataSource);
-	  
-	  return db;
-	 }
+	public PersistentTokenRepository persistentTokenRepository() {
+		JdbcTokenRepositoryImpl db = new JdbcTokenRepositoryImpl();
+		db.setDataSource(dataSource);
+
+		return db;
+	}
 }
