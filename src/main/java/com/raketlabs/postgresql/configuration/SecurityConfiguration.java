ebcea27631999a5c0349f78da3d64ab61836f742
Mark Oral Hernandez <blackcoder41@gmail.com>
2019-02-25 16:06:38 +0800

    remove my email

--- src/main/java/com/raketlabs/postgresql/configuration/SecurityConfiguration.java
+++ src/main/java/com/raketlabs/postgresql/configuration/SecurityConfiguration.java
@@ -38,7 +38,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
-		// blackcoder@gmail.com
 		
 		http.authorizeRequests()
 
